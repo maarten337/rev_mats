52352375d88c1826ed3923549489c9150b64a6fc
maarten <maarten_thejager@hotmail.com>
2026-06-03 22:58:50 +0200

    tmp

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def telegram_notify(message):
 
 
 
-products = ['Oil 3X Daily', ' Oil 3x Short','Silver 3X Daily',' Silver 3x Short']
+products = ['Oil 3X Daily', ' Oil 3x Short','Silver 3X Daily',' Silver 3x Short',]
 isin = ['IE00BMTM6B32','XS2819844387','XS3306516876','XS3306517924']
 
 
@@ -44,6 +44,12 @@ oil_short = Instrument('XS2819844387', request)
 
 silver_long = Instrument('IE00BMTM6B32', request)
 silver_short = Instrument('XS2819844387', request)
+
+#hkex setup
+xiaomi = Instrument('KYG9830T1067', request)
+byd = Instrument('CNE100000296', request)
+lenovo = Instrument('HK0992009065', request)
+geely = Instrument('KYG3777B1032', request)
 
 
 oil_short_open = 1.5468
@@ -69,11 +75,21 @@ silver_long_qnty = 5000/silver_long_open
 
 
 
+def write_log(instrument,name):
+    log = f"{time_str},{name},{instrument.bid},{instrument.ask},{instrument.data} \n"
+    with open(f"ticker_data_log/hkex/{name}/mats_logs_{date_str}.txt", "a") as file:
+        file.write(log)
+
 def pl_oil( l_bid, l_ask, s_bid, s_ask):
-    pl_bid = oil_short_qnty * (s_bid - oil_short_entry) + oil_long_qnty * (l_bid - oil_long_entry)
-    pl_ask = oil_short_qnty * (s_ask - oil_short_entry) + oil_long_qnty * (l_ask - oil_long_entry)
-    pl_long_bid_short_ask = oil_short_qnty * (s_ask - oil_short_entry) + oil_long_qnty * (l_bid - oil_long_entry)
-    pl_long_ask_short_bid = oil_short_qnty * (s_bid - oil_short_entry) + oil_long_qnty * (l_ask - oil_long_entry)
+    long_bid = float(str(l_bid).replace(",", "."))
+    long_ask = float(str(l_ask).replace(",", "."))
+    short_bid = float(str(s_bid).replace(",", "."))
+    short_ask = float(str(s_ask).replace(",", "."))
+
+    pl_bid = oil_short_qnty * (short_bid - oil_short_entry) + oil_long_qnty * (long_bid - oil_long_entry)
+    pl_ask = oil_short_qnty * (short_ask - oil_short_entry) + oil_long_qnty * (long_ask - oil_long_entry)
+    pl_long_bid_short_ask = oil_short_qnty * (short_ask - oil_short_entry) + oil_long_qnty * (long_bid - oil_long_entry)
+    pl_long_ask_short_bid = oil_short_qnty * (short_bid - oil_short_entry) + oil_long_qnty * (long_ask - oil_long_entry)
 
     r3 = f"oil_long_bid_short_ask {pl_long_bid_short_ask}"
     r4 = f"oil_long_ask_short_bid {pl_long_ask_short_bid}"
@@ -81,10 +97,15 @@ def pl_oil( l_bid, l_ask, s_bid, s_ask):
 
 
 def pl_silver( l_bid, l_ask, s_bid, s_ask):
-    pl_bid = silver_short_qnty * (s_bid - silver_short_entry) + silver_long_qnty * (l_bid - silver_long_entry)
-    pl_ask = silver_short_qnty * (s_ask - silver_short_entry) + silver_long_qnty * (l_ask - silver_long_entry)
-    pl_long_bid_short_ask = silver_short_qnty * (s_ask - silver_short_entry) + silver_long_qnty * (l_bid - silver_long_entry)
-    pl_long_ask_short_bid = silver_short_qnty * (s_bid - silver_short_entry) + silver_long_qnty * (l_ask - silver_long_entry)
+    long_bid = float(str(l_bid).replace(",", "."))
+    long_ask = float(str(l_ask).replace(",", "."))
+    short_bid = float(str(s_bid).replace(",", "."))
+    short_ask = float(str(s_ask).replace(",", "."))
+
+    pl_bid = silver_short_qnty * (short_bid - silver_short_entry) + silver_long_qnty * (long_bid - silver_long_entry)
+    pl_ask = silver_short_qnty * (short_ask - silver_short_entry) + silver_long_qnty * (long_ask - silver_long_entry)
+    pl_long_bid_short_ask = silver_short_qnty * (short_ask - silver_short_entry) + silver_long_qnty * (long_bid - silver_long_entry)
+    pl_long_ask_short_bid = silver_short_qnty * (short_bid - silver_short_entry) + silver_long_qnty * (long_ask - silver_long_entry)
 
     r3 = f"silver_long_bid_short_ask {pl_long_bid_short_ask}"
     r4 = f"silver_long_ask_short_bid {pl_long_ask_short_bid}"
@@ -100,7 +121,9 @@ while True:
         time_str = datetime.now().strftime("%Y%m%d-%H%M%S")
         x = f"{time_str},{products[0]},{isin[0]},{oil_long.ask}, {oil_long.bid}, {oil_long.data} \n"
         y = f"{time_str},{products[1]},{isin[1]},{oil_short.ask}, {oil_short.bid}, {oil_short.data} \n"
-        print(f"OIL:{pl_oil(float(oil_long.bid), float(oil_long.ask), float(oil_short.bid), float(oil_short.ask))} \t SILVER: {pl_silver(float(silver_long.bid), float(silver_long.ask), float(silver_short.bid), float(silver_short.ask))}")
+
+
+        print(f"OIL:{pl_oil(oil_long.bid, oil_long.ask, oil_short.bid, oil_short.ask)} \t SILVER: {pl_silver(silver_long.bid, silver_long.ask, silver_short.bid, silver_short.ask)}")
         date_str = datetime.now().strftime("%Y%m%d")
         with open(f"ticker_data_log/wti_oil/mats_logs_{date_str}.txt", "a") as file:
             file.write(x)
@@ -111,10 +134,17 @@ while True:
         with open(f"ticker_data_log/wti_silver/mats_logs_{date_str}.txt", "a") as file:
             file.write(z)
             file.write(v)
-        if pl_oil(float(oil_long.bid), float(oil_long.ask), float(oil_short.bid), float(oil_short.ask))[1] > 30 and oil_bought_ind is True and time.time() - oil_note_ts > 300:
-            telegram_notify(f"SELL OIL AT {datetime.now()}: {pl_oil(float(oil_long.bid), float(oil_long.ask), float(oil_short.bid), float(oil_short.ask))}")
+
+        write_log(lenovo,'lenovo')
+        write_log(xiaomi,'xiaomi')
+        write_log(byd,'byd')
+        write_log(geely,'geely')
+
+
+        if pl_oil(oil_long.bid, oil_long.ask, oil_short.bid, oil_short.ask)[1] > 30 and oil_bought_ind is True and time.time() - oil_note_ts > 300:
+            telegram_notify(f"SELL OIL AT {datetime.now()}: {pl_oil(oil_long.bid, oil_long.ask, oil_short.bid, oil_short.ask)}")
             oil_note_ts = time.time()
 
-    except:
-        print("error")
+    except Exception as e:
+        print(e)
 
